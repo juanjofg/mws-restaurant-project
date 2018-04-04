16abe2dd02dd7570350c25331dde1ac92ea14e3b
Juanjo Fernández <juanjo@nextail.co>
2018-04-04 09:55:23 +0200

    Add grunt task to transform images and get needed sizes and qualities for the restaurant list Add images folder to gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 480,
+            suffix: '_1x',
+            quality: 50
+          }, {
+            width: 480,
+            suffix: '_2x',
+            quality: 100
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'img/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
