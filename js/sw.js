d222cc08538ec585326dfec36aef5e3aa6084819
Juanjo Fernández <juanjo@nextail.co>
2018-07-26 00:30:08 +0200

    Feat: Add svg files to cache and remove unused stuff

--- js/sw.js
+++ js/sw.js
@@ -19,6 +19,9 @@ self.addEventListener('install', function(event) {
         '/img/8-480_1x.jpg',
         '/img/9-480_1x.jpg',
         '/img/10-480_1x.jpg',
+        '/img/fav_1.svg',
+        '/img/fav_2.svg',
+        '/img/map.svg',
         'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700'
       ]);
     })
@@ -49,9 +52,15 @@ self.addEventListener('fetch', function(event) {
   // images from cache
   let imageRegex = /\/img\/([d\-\d\_d])*\x.(?:jpg)/;
   if (imageRegex.test(event.request.url)) {
-    let lowRes = '480_1x';
     let img = event.request.url.replace(/-([\d\_d])*\x/, '-480_1x');
     event.respondWith(caches.match(img));
+    return;
+  }
+
+  // Intercept svg files and return from cache
+  if (event.request.url.indexOf('svg') > 0) {
+    let svgIdx = event.request.url.indexOf('/img');
+    event.respondWith(caches.match(event.request.url.substr(svgIdx)));
     return;
   }
 
